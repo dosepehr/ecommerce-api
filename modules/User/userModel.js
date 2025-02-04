fc568904c5243fabf1a0af7214bd8d2ddab62a89
dosepehr <dosepehr@gmail.com>
2025-02-04 13:19:29 +0330

    add User model

---
+++ modules/User/userModel.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        role: {
+            type: String,
+            enum: ['user', 'teacher', 'admin'],
+            default: 'user',
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        passwordChangedAt: Date,
+        passwordResetToken: String,
+        passwordResetExpires: Date,
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+        timestamps: true,
+    },
+);
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10,
+        );
+
+        return JWTTimestamp < changedTimestamp;
+    }
+
+    // False means NOT changed
+    return false;
+};
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
