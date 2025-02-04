c8d676cb961529e1f563c18d3990456643c1b0e3
dosepehr <dosepehr@gmail.com>
2025-02-04 15:30:52 +0330

    add token protecting routes

--- modules/Auth/authRouter.js
+++ modules/Auth/authRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { sendOtp, verifyOtp } = require('./authController');
+const { sendOtp, verifyOtp, protect } = require('./authController');
 
 const authRouter = express.Router();
 
 authRouter.post('/sendOtp', sendOtp);
-authRouter.post('/verifyOtp', verifyOtp);
+authRouter.post('/verifyOtp', protect, verifyOtp);
 
 module.exports = authRouter;
