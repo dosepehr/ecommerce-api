24d821cc5168dc1cebc9f7dffe16f8b9ec7c9e1a
dosepehr <dosepehr@gmail.com>
2025-02-05 10:33:00 +0330

    add shop model

---
+++ modules/Shop/shopModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const shopSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        cityId: {
+            type: Number,
+            required: true,
+        },
+        seller: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+        timestamps: true,
+    }
+);
+
+const Shop = mongoose.model('Shop', shopSchema);
+
+module.exports = Shop;
